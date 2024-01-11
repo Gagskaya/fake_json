f0b479d660f900938d48de4c155549b6d763e1a7
gagskaya <alisxan006@mail.ru>
2024-01-11 22:03:34 +0300

    fix

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 const jsonServer = require("json-server");
+const auth = require("json-server-auth");
 const server = jsonServer.create();
 const router = jsonServer.router("db.json");
-const midlewares = jsonServer.defaults();
+// const midlewares = jsonServer.defaults();
 const port = 3002;
 
-server.use(midlewares);
+server.db = router.db;
+
+// server.use(midlewares);
+server.use(auth);
 server.use(router);
 
 server.listen(port);
