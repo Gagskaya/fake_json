e317f32ccae0187fe6bfb87c61903b193b286868
gagskaya <alisxan006@mail.ru>
2024-01-11 22:13:47 +0300

    cors fix

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const jsonServer = require("json-server");
 const auth = require("json-server-auth");
 const server = jsonServer.create();
 const router = jsonServer.router("db.json");
+const cors = require("cors");
 // const midlewares = jsonServer.defaults();
 const port = 3002;
 
@@ -10,5 +11,6 @@ server.db = router.db;
 // server.use(midlewares);
 server.use(auth);
 server.use(router);
+server.use(cors);
 
 server.listen(port);
